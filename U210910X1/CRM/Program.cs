3dac1e422f004b73f72fb22d4709c5dca31908b8
JohanK <klausenjohan23@gmail.com>
2021-09-10 12:07:50 +0200

    skapat en enkel meny

--- U210910X1/CRM/Program.cs
+++ U210910X1/CRM/Program.cs
@@ -6,7 +6,30 @@ namespace CRM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+
+                Console.WriteLine("1-Skapa ny kund, 2-Visa antal kunder, 3-Visa lista över alla kunder, 4-Avsluta");
+                string orden = Console.ReadLine();
+
+                if (orden == "1")
+                {
+                    Console.WriteLine("Förnamn och efternamn");
+                }
+                if (orden == "2")
+                {
+                    Console.WriteLine("visa antal kunder");
+                }
+                if (orden == "3")
+                {
+
+                }
+                if (orden == "4")
+                {
+                    break;
+                }
+            }
         }
     }
 }
